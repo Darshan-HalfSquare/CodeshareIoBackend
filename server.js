87987f9a6f57ace6d91f2b74020a70fca7ea2d73
darshan-halfsquare <iamdhameliya@gmail.com>
2022-02-22 10:26:57 +0530

    video call isuue solved

--- server.js
+++ server.js
@@ -8,16 +8,13 @@ const io = socket(server);
 
 const users = {};
 
+const VideCallUser = {};
+
 const socketToRoom = {};
 
 io.on("connection", (socket) => {
   socket.on("join room", (roomID) => {
     if (users[roomID]) {
-      const length = users[roomID].length;
-      if (length === 4) {
-        socket.emit("room full");
-        return;
-      }
       users[roomID].push(socket.id);
     } else {
       users[roomID] = [socket.id];
@@ -27,19 +24,36 @@ io.on("connection", (socket) => {
     socket.emit("all users", usersInThisRoom);
   });
 
-  // socket.on("sending signal", (payload) => {
-  //   io.to(payload.userToSignal).emit("user joined", {
-  //     signal: payload.signal,
-  //     callerID: payload.callerID,
-  //   });
-  // });
+  socket.on("start Video call", (roomID) => {
+    if (VideCallUser[roomID]) {
+      const length = VideCallUser[roomID].length;
+      if (length === 4) {
+        socket.emit("room full");
+        return;
+      }
+      VideCallUser[roomID].push(socket.id);
+    } else {
+      VideCallUser[roomID] = [socket.id];
+    }
+    const usersInThisRoom = VideCallUser[roomID].filter(
+      (id) => id !== socket.id
+    );
+    socket.emit("all users", usersInThisRoom);
+  });
 
-  // socket.on("returning signal", (payload) => {
-  //   io.to(payload.callerID).emit("receiving returned signal", {
-  //     signal: payload.signal,
-  //     id: socket.id,
-  //   });
-  // });
+  socket.on("sending signal", (payload) => {
+    io.to(payload.userToSignal).emit("user joined", {
+      signal: payload.signal,
+      callerID: payload.callerID,
+    });
+  });
+
+  socket.on("returning signal", (payload) => {
+    io.to(payload.callerID).emit("receiving returned signal", {
+      signal: payload.signal,
+      id: socket.id,
+    });
+  });
 
   socket.on("receive dataText", (payload) => {
     users[payload.room].map((item) => {
@@ -51,10 +65,10 @@ io.on("connection", (socket) => {
 
   socket.on("disconnect", () => {
     const roomID = socketToRoom[socket.id];
-    let room = users[roomID];
+    let room = VideCallUser[roomID];
     if (room) {
       room = room.filter((id) => id !== socket.id);
-      users[roomID] = room;
+      VideCallUser[roomID] = room;
     }
     socket.broadcast.emit("user left", socket.id);
   });
